28c10f120ac93edd916cb317fcf109f385b5937c
Luis David Fuentes Juvera <luis.fuju@outlook.com>
2026-08-02 18:15:43 -0600

    Dockerfile Backend
    Adding Dockerfile for the server REST API.
    Two-stage build on scratch container.
    Adding ENV variables for API port and
    authorized origins.

--- backend/src/server/main.go
+++ backend/src/server/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"os"
 	"fmt"
+	"strings"
 	"net/http"
 	"server/internal/server"
 )
 
 func main() {
-	fmt.Println("Server starting on :8080")
-	fmt.Println(http.ListenAndServe(":8080", server.New()))
+	var port string = os.Getenv("SERVER_PORT")
+	var allowedOrigins []string = strings.Split(os.Getenv("ALLOWED_ORIGINS"), ",")
+	if port != "" {
+		portString := fmt.Sprintf(":%s", port)
+		fmt.Printf("Server starting on %s\n", portString)
+		fmt.Println(http.ListenAndServe(portString, server.New(allowedOrigins)))
+	}
 }
